834b956e0dfdd2759b122d65a3fc0b12b876a788
Dina <dinagiacobbi@gmail.com>
2022-09-29 10:58:50 -0800

    Adding new classes

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-const button = document.getElementById('button')
-const toasts = document.getElementById('toasts')
+const button = document.getElementById("button")
+const toasts = document.getElementById("toasts")
 
 const messages = [
-    "You're brilliant!",
-    "Take a day off!",
-    "You're smile lights up the room",
-    "Watch a funny YouTube video",
-    "Think sunny thoughts"
+  "You're brilliant!",
+  "Take a day off!",
+  "You're smile lights up the room",
+  "Watch a funny YouTube video",
+  "Think sunny thoughts",
 ]
 
-button.addEventListener('click', createNotification)
+const types = ["info", "success", "error"]
 
-function createNotification () {
-  const notifcation = document.createElement('div')
-  notifcation.classList.add('toast')
+button.addEventListener("click", () => createNotification())
 
-  notifcation.innerHTML = getRandomMessage()
+function createNotification(message = null, type = null) {
+  const notifcation = document.createElement("div")
+  notifcation.classList.add("toast")
+  notifcation.classList.add(type ? type : getRandomType())
+
+  notifcation.innerHTML = message ? message : getRandomMessage()
 
   toasts.append(notifcation)
 
   setTimeout(() => {
     notifcation.remove()
-  }, 3000);
+  }, 2500)
 }
 
 function getRandomMessage() {
-    return messages[Math.floor(Math.random()* messages.length)]
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)]
 }
